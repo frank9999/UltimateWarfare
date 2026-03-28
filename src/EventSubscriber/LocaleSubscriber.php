d387aaddb0042fd465c08ce5df5ad497b005c9a9
Frank Schreuder <admin@frankprojects.com>
2026-03-28 23:04:37 +0100

    Rebrand the main website layout

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -34,6 +34,7 @@ class LocaleSubscriber implements EventSubscriberInterface
         $locale = $request->query->get('_locale');
         if ($locale !== null && in_array($locale, $this->validLocales, true)) {
             $request->getSession()->set('_locale', $locale);
+            $request->setLocale($locale);
         } else {
             // if no explicit locale has been set on this request, use one from the session
             /** @var string $sessionLocale */
